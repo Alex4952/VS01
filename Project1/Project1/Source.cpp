604cda8a7100ecfb8582306fa886d12f63ed2579
Alex4952 <audenver@naver.com>
2020-07-10 18:39:45 +0900

    Add function Add()

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #pragma warning (disable : 4996 6031)
 using namespace std;
+int Add(int x, int y) {
+	return x + y;
+}
 int main() {
 	cout << "Hello World" << endl;
+	cout << Add(10, 3) << endl;
 	return 0;
 }
