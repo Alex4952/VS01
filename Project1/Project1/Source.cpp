7c6ebd81c3d3c0c2c3553ad562e86f5e16ef67b8
Alex4952 <keyzoo@gmail.com>
2020-07-12 06:47:47 +0900

    Modify main (Mul() 출력 추가)

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #pragma warning (disable : 4996 6031)
 using namespace std;
+int Mul(int, int);
 int Add(int x, int y) {
 	return x + y;
 }
@@ -11,5 +12,6 @@ int main() {
 	cout << "Hello World" << endl;
 	cout << Add(10, 3) << endl;
 	cout << Sub(10, 3) << endl;
+	cout << Mul(10, 3) << endl;
 	return 0;
 }
